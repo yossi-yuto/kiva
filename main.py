6c8d4438ae9bb4a3769bece137af7d9f1464f8bd
yossi-yuto <92134254+yossi-yuto@users.noreply.github.com>
2022-01-30 14:34:11 +0900

    Update main.py
    feat:ゼロパディングと画像とデータフレームが対応したデータセットを作成

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ import glob
 import os 
 import pdb
 import matplotlib.pyplot as plt
-import ZipMake
+from ZipMake import ZipMake
+from ZipMake import OpenPickle
+
 import pandas as pd
 import random
 import time
@@ -80,61 +82,26 @@ if not os.path.exists(dir_train_name):
 if not os.path.exists(dir_test_name):
   os.makedirs(dir_test_name)
 
+#パスのリスト化
 #=========================================================================
 zip_path = list([zip_train_path,zip_test_path])
 csv_path = list([csv_train_path,csv_test_path])
 df_filename = list([df_train_filename,df_test_filename])
 dir_name = list([dir_train_name,dir_test_name])
+#=========================================================================
 
-
-# parameter setting
-visual_path = 'visualization'
-checkpoint_path = 'checkpoint'
-checkpoint_file = 'weights_nn.ckpt'
-
-#ハイパーパラメータの設定
-input_shape = (224, 224, 3)
-BATCH_SIZE = 128
-Epochs = 40
-patch_size = (8, 8)  # 2-by-2 sized patches
-dropout_rate = 0.03  # Dropout rate
-num_heads = 8  # Attention heads
-embed_dim = 64  # Embedding dimension
-num_mlp = 256  # MLP layer size
-qkv_bias = True  # Convert embedded patches to query, key, and values with a learnable additive value
-window_size = 2  # Size of attention window
-shift_size = 1  # Size of shifting window
-image_dimension = 224  # Initial image size
-
-num_patch_x = input_shape[0] // patch_size[0]
-num_patch_y = input_shape[1] // patch_size[1]
-
-learning_rate = 1e-3
-batch_size = 128
-num_epochs = 40
-validation_split = 0.1
-weight_decay = 0.0001
-label_smoothing = 0.1
-
-
-# ディレクトリが存在しない場合は作成
-if not os.path.exists(visual_path):
-    os.makedirs(visual_path)
-if not os.path.exists(checkpoint_path):
-    os.makedirs(checkpoint_path)
-#----------------------------
-
-
+#データ作成
+#==========================================================================
 if isMakeData:
   # zipmakeインスタンスを作成
   zipmake = ZipMake.ZipMake(current_dir,zip_path,csv_path,df_filename,dir_name)
 
   # 訓練データを作成（mkfrm_flgはdataframe.pikcleを作成するときTrue,default:False)
-  zipmake.make_train_data(mkfrm_flag=False)
+  zipmake.make_train_data(mkfrm_flag=True)
 
   # テストデータを作成（mkfrm_flgはdataframe.pikcleを作成するときTrue,default:False)
-  zipmake.make_test_data(mkfrm_flag=False)
-
+  zipmake.make_test_data(mkfrm_flag=True)
+#===========================================================================
 
 
 
@@ -147,9 +114,6 @@ with open(df_filename[0],'rb') as train, open(df_filename[1],'rb') as test:
 '''
 df_train = pd.read_csv(csv_path[0])
 df_test = pd.read_csv(csv_path[1])
-
-
-## データの前処理
 
 #欠損値を消す(TOWN_NAMEとTAGS）
 #TOWN_NAMEを取り入れるより、Countrycodeで判別できると判断したため、削除
@@ -188,70 +152,65 @@ df_train['CURRENCY_POLICY'] = df_train['CURRENCY_POLICY'].replace({'shared':1, '
 df_train['REPAYMENT_INTERVAL'] = df_train['REPAYMENT_INTERVAL'].replace({'monthly':1, 'bullet':2, 'irregular':3})
 #融資形態
 df_train['DISTRIBUTION_MODEL'] = df_train['DISTRIBUTION_MODEL'].replace({'field_partner':1, 'direct':2})
-
-#================================================================
-
-
-# pickleファイルパスから画像データを生成
-def open_pkl_image(filepath,rand_num = 50):
-  '''
-  feat：100枚1セットのファイルパスをrand_num*100枚分だけランダムに抽出し、内部でシャッフルしデータを生成
-  引数：ファイルパスのリスト
-  返り値：画像のid,tf.data.Dataset型のデータ
-  defaultでは50なので、5000枚の画像が入ったtf.data.Datasetを返す
-  '''
-  filepath = random.sample(filepath,rand_num) # rand_num分だけファイルパスをリストとして保存
-  firstLoop = True  
-  for path in filepath:    
-    with open(path,'rb') as f:
-      if firstLoop:
-        x = pickle.load(f)
-        loan_amount, img_array, loan_id = x
-        loan_amount = tf.data.Dataset.from_tensor_slices(loan_amount)
-        img_array = img_array / 255
-        img_array = tf.data.Dataset.from_tensor_slices(tf.cast(img_array, tf.float64))
-        image_label = tf.data.Dataset.zip((img_array,loan_amount))
-        
-        df_data = pd.DataFrame(loan_id,columns=['LOAN_ID'])       
-        firstLoop = False
-        
-      else:        
-        y = pickle.load(f)
-        loan_amount, img_array, loan_id = y
-        loan_amount = tf.data.Dataset.from_tensor_slices(loan_amount)
-        img_array = img_array / 255
-        img_array = tf.data.Dataset.from_tensor_slices(tf.cast(img_array, tf.float64))
-        image_label_ds = tf.data.Dataset.zip(( img_array,loan_amount))
-        image_label = image_label.concatenate(image_label_ds)
-        df_data_tmp = pd.DataFrame(loan_id,columns=['LOAN_ID'])
-        df_data = pd.concat([df_data,df_data_tmp],axis=0)
-          
-  return df_data, image_label
+#===========================================================================================================
 
 
+
+# 訓練データの生成-----------------------------
 # 画像の前処理
+# 画像：tf.data.Dataset / テーブルデータ：df_train
 #================================================================
-
 # 画像データのpickleパスを訓練、テストのフォルダから全部取得
 pickle_file = glob.glob(os.path.join(dir_name[0], '*.pickle'))     
 pickle_test_file = glob.glob(os.path.join(dir_name[1],'*.pickle'))
-
-
-
-# 訓練データ----------------------------------------
-# 画像：tf.data.Dataset / テーブルデータ：df_train
-id , image = open_pkl_image(pickle_file)
+open = OpenPickle(pickle_file)
+id , image_and_label = open.open_pkl_image()
 df_train = pd.merge(df_train,id,on='LOAN_ID')
-
-
+#================================================================
 
 pdb.set_trace()
 
+# モデル学習
+#==================================================
+'''
+・画像データのモデル
+   model.fit(image_and_label) 画像と正解値のセットなので分離は不要
+
+・テーブルデータのモデル
+   model.fit(df_train)  データフレームを訓練データ
+
+・マルチモーダルなモデル
+   ?.fit(image_and_label,df_train)  マルチモーダルなモデルについては詳しくないのでよろしくお願いいたします。
+
+などのモデルはここに記述↓
+
+'''
+#===================================================
 
 
 
+# テストデータ--------------------------------------
+#================================================================
+for path in pickle_test_file:
+  id, image_test = open.open_test_pkl_image(path)
+  df_test = pd.merge(df_test,id,on='LOAN_ID')
 
+  # 学習モデルに適用し、予測する
+  # 1.画像用モデルには model.predict(image_test)
+  # 2.テーブルデータ用のモデルには model.predict(df_test)
+  # (3).マルチモーダルなモデルを使用する場合は、そのモデルに対して  model.predict(image_test,df_test) 
 
+  # idにはLOAN_IDが格納されているのでsample_submission.csvにはfor分で逐次的に予測したデータを書き込んでいく。
+  '''
+  submit_path = os.path.join(current_dir, "sample_submission.csv")
+  submit = pd.read_csv(submit_path)
+  submit = pd.merge(id,summit,on='LOAN_ID')
+  submit['LOAN_AMOUNT'] = pred  ←予測したデータ
+  submit.to_csv(submit_path)  ←提出用データの上書き保存 
+  
+  '''
+#====================================================================
+  
 
-
+pdb.set_trace()
 
